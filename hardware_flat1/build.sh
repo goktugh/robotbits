81ff580538f84f0ea85fa54a4224ab14e2f9a56d
Mark Robson <mark.robson@kaspersky.com>
2018-03-09 08:20:45 +0000

    build and ignore

--- hardware_flat1/build.sh
+++ hardware_flat1/build.sh
@@ -1,7 +1,23 @@
+set -e
+set -x
+
 # Generate flat1_tinyholes.dxf with very small holes, for
 # drilling out mechanically.
 
-
 # We can laser etch the outline and the holes, so that the drill can be guided.
-openscad flat1.scad -D override_hole_r=0.5 -o flat1_tinyholes.dxf
+mkdir -p output
+openscad flat1.scad -D override_hole_r=0.5 -o output/flat1_tinyholes.dxf
+openscad flat1.scad -D override_hole_r=0.5 -o output/flat1_tinyholes.svg
+
+# Wheel
+
+openscad wheel_with_insert.scad -o output/wheel_with_insert.stl
+
+# Flipper attach
+openscad flipper.scad -D generate_flipper_main=false -o output/flipper_attach.stl
+openscad flipper.scad -o output/flipper.stl
+
+# Motor mount
+openscad motor_mount_correct.scad -o output/motor_mount.stl
+openscad motor_mount_cutout.scad -o output/motor_mount_cutout.stl
 
