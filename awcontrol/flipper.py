f9e9b7452e6eb4078b00686dbf7633db05eaabbb
Mark R <markxr@gmail.com>
2018-04-28 18:53:37 +0000

    flipper test program

---
+++ awcontrol/flipper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import time
+import os
+
+pigpio_f = None
+
+GPIO_FLIP_ENABLE = 16
+GPIO_FLIP_0 = 20 # Enable to flip up
+GPIO_FLIP_1 = 21 # enable to flip down
+
+def init_pigpio():
+    global pigpio_f
+    pigpio_f = open('/dev/pigpio', 'wt')
+
+def set_flipper(direction, duty):
+    # Duty = 0...255
+    p0 = (direction > 0)
+    p1 = (direction < 0)
+    pigpio_f.write("p {} {}\n".format(GPIO_FLIP_ENABLE, int(duty)))
+    pigpio_f.write("w {} {}\n".format(GPIO_FLIP_0, int(p0)))
+    pigpio_f.write("w {} {}\n".format(GPIO_FLIP_1, int(p1)))
+    pigpio_f.flush()
+
+
+if __name__ == '__main__':
+    init_pigpio()
+    print("Flipping...")
+    set_flipper(1,255)
+    time.sleep(0.20)
+    print("waiting...")
+    set_flipper(0,0)
+    time.sleep(0.5)
+    print("retracting...")
+    # Retract slowly.
+    set_flipper(-1,64)
+    time.sleep(5)
+    print("end")
+    set_flipper(0,0)
+
