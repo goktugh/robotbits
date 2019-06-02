c088b0f48bc70c7535e9cefdbdf4eb8a220bde22
Mark Robson <mark.robson@kaspersky.com>
2019-06-02 12:37:15 +0100

    build

--- hardware_stacie2/build.sh
+++ hardware_stacie2/build.sh
@@ -6,7 +6,7 @@ for name in sidepanel outerpanel idle_gear_2d lid; do
     openscad $name.scad -o $outdir/$name.dxf
 done
 
-for name in flip_coupler idle_gear wheel_gear wheel_gear_bearing shell; do
+for name in tyre_mould flip_coupler idle_gear wheel_gear wheel_gear_bearing shell; do
 
     openscad $name.scad -o $outdir/$name.stl
 done
