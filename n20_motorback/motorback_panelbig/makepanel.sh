01b0455476864af590207a7cd36c3c67199fc1ee
Mark Robson <markxr@gmail.com>
2021-02-27 09:46:44 +0000

    Panel of 16

---
+++ n20_motorback/motorback_panelbig/makepanel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -x
+java -jar /home/mark/unpack/kicad-util/target/kicadutil-1.0-SNAPSHOT.jar \
+    pcb -f panelbig.kicad_pcb panel --inset=-0.1 --width=4.0 --pitch 0.8 --hole 0.6
