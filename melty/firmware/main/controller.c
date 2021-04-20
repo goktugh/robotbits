143e129452a33f52a1441e65e7a855202eddb030
Mark Robson <markxr@gmail.com>
2021-04-20 20:31:40 +0000

    firmware shenanigans

--- melty/firmware/main/controller.c
+++ melty/firmware/main/controller.c
@@ -8,11 +8,26 @@
 static void controller_event_cb( ps3_t ps3, ps3_event_t event )
 {
     // Event handling here...
+    if (event.button_down.cross) {
+        printf("PS3: cross button down\n");
+    }
+}
+
+static void controller_connected_cb( uint8_t is_connected )
+{
+    if (is_connected) {
+        printf("PS3 controller connected ok\n");
+        ps3SetLed(1);
+    } else {
+        printf("PS3 controller disconnected\n");        
+    }
 }
 
 void controller_init()
 {
     ps3SetEventCallback(controller_event_cb);
+    ps3SetConnectionCallback( controller_connected_cb );
+
     ps3Init();
     
     printf("Local bt address:");
